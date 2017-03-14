8e73df72e4c5e4da6722855e34a82355d10c8402
Stephen Sullivan <catalyst7193@gmail.com>
2017-03-14 00:10:46 -0400

    DEV:  Renamed package from email to github username. Added top level build.gradle

--- edge-gateway/src/main/java/com/github/catalyst7193/ZuulWizardConfiguration.java
+++
@@ -1,9 +0,0 @@
-package com.github.catalyst7193;
-
-import io.dropwizard.Configuration;
-
-/**
- * Created by catal on 3/13/2017.
- */
-public class ZuulWizardConfiguration extends Configuration {
-}
